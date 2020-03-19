ad06436d45544bfa4c479cf0b3d22e3a824c4c34
apcsp-student <62392101+apcsp-student@users.noreply.github.com>
2020-03-19 13:53:26 -0400

    Deleted other characters; changed greeting

--- main.ts
+++ main.ts
@@ -36,26 +36,12 @@ function greeting () {
 . . . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
     mySprite2.setPosition(78, 74)
-    mySprite.say("Here is my")
-    mySprite2.say("work")
+    mySprite.say("This is ")
+    mySprite2.say("Keroppi")
 }
 function sayHello () {
-    for (let index = 0; index < 10000; index++) {
-        let myMelody: Sprite = null
-        let badtzmaru: Sprite = null
-        let chococat: Sprite = null
-        let helloKitty: Sprite = null
-        helloKitty.say("hello", 1000)
-        pause(1000)
-        keroppi.say("hello", 1000)
-        pause(1000)
-        chococat.say("hello", 1000)
-        pause(1000)
-        badtzmaru.say("hello", 1000)
-        pause(1000)
-        myMelody.say("hello", 1000)
-        pause(1000)
-    }
+    keroppi.say("hello", 1000)
+    pause(1000)
 }
 let mySprite2: Sprite = null
 let mySprite: Sprite = null
@@ -182,6 +168,7 @@ d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d 
 d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d 
 d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d d 
 `)
+greeting()
 keroppi = sprites.create(img`
 . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . f f f f f . . . . . f f f f f . . . . . 
@@ -206,3 +193,28 @@ keroppi = sprites.create(img`
 . . . . . . . . . . . . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
 keroppi.setPosition(45, 25)
+sayHello()
+let keroppiBig = sprites.create(img`
+. . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . f f f f f . . . . . f f f f f . . . . . 
+. . . . f 1 1 1 1 1 f . . . f 1 1 1 1 1 f . . . . 
+. . . f 1 1 1 1 1 1 1 f . f 1 1 1 1 1 1 1 f . . . 
+. . f 1 1 1 1 1 1 f 1 1 f 1 1 f 1 1 1 1 1 1 f . . 
+. . f 1 1 1 1 1 f f f 1 f 1 f f f 1 1 1 1 1 f . . 
+. . f 1 1 1 1 1 f f f 1 f 1 f f f 1 1 1 1 1 f . . 
+. . f 1 1 1 1 1 1 f 1 1 f 1 1 f 1 1 1 1 1 1 f . . 
+. . . f 1 1 1 1 1 1 1 f 7 f 1 1 1 1 1 1 1 f . . . 
+. . f 7 f 1 1 1 1 1 f 7 7 7 f 1 1 1 1 1 f 7 f . . 
+. f 7 7 7 f f f f f 7 7 7 7 7 f f f f f 7 7 7 f . 
+. f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f . 
+. f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f . 
+. f 7 7 7 3 3 3 7 7 7 7 7 7 7 7 7 3 3 3 7 7 7 f . 
+. f 7 7 7 3 3 3 7 7 7 7 7 7 7 7 7 3 3 3 7 7 7 f . 
+. . f 7 7 3 3 3 7 7 7 7 7 7 7 7 7 3 3 3 7 7 f . . 
+. . f 7 7 7 7 7 7 f 7 7 7 7 7 f 7 7 7 7 7 7 f . . 
+. . . f 7 7 7 7 7 7 f 7 7 7 f 7 7 7 7 7 7 f . . . 
+. . . . f 7 7 7 7 7 7 f 7 f 7 7 7 7 7 7 f . . . . 
+. . . . . f f f f f f f f f f f f f f f . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+keroppiBig.setPosition(105, 95)
